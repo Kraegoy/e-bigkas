370aec2700a83c30d5208a4bb4287efdbbf11017
kraeg <avilakraeg@gmail.com>
2024-10-05 16:59:39 +0800

    a

--- ebigkasProject/settings.py
+++ ebigkasProject/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-uuky)(k2i(ifs$4wgz*e^^o&^s8jpp(b*n6lb#&#s6ee*-c(%2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
