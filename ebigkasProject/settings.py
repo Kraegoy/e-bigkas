dbc38f697814c678750c0a968a882ee2d6efa404
kraeg <avilakraeg@gmail.com>
2024-10-05 15:50:58 +0800

    a

--- ebigkasProject/settings.py
+++ ebigkasProject/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '54.242.122.41', 'ebigkas.com', 'wwww.ebigkas.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '54.198.184.25', 'ebigkas.com', 'wwww.ebigkas.com']
 
 
 # Application definition
